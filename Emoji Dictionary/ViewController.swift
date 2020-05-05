2f1c6e45b4b1697111d852a0bb7b94e514c3f2c7
Lia Pennefather <lia.pennefather@later.com>
2020-05-05 14:38:18 -0700

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by coop on 2020-05-05.
+//  Copyright © 2020 coop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
